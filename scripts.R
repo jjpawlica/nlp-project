b8213bd6b18962d4bb7112a03fe14e6905a49605
Jakub Pawlica <jakub.pawlica@gmail.com>
2019-06-12 18:50:20 +0200

    tworzenie macierzy częstości

--- scripts.R
+++ scripts.R
@@ -57,8 +57,6 @@ vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(171), "")
 vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(190), "")
 vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(190), "")
 
-
-
 # Wstępne przetwarzanie: lematyzacja
 
 polish <- dictionary(lang = "pl_PL", affix = NULL, cache = TRUE)
@@ -80,7 +78,58 @@ lemmatize <- function(text) {
 
 vcorpus <- tm_map(vcorpus, content_transformer(lemmatize))
 
-# Wyświetlanie informacji o korpusie
+# Usunięcie rozszerzeń z nazw dokumentów w korpusie
+
+cutExtensions <- function(document) {
+  meta(document, "id") <- gsub(
+    pattern="\\.txt$",
+    "",
+    meta(document, "id")
+  )
+  return(document)
+}
+
+vcorpus <- tm_map(vcorpus, cutExtensions)
+
+# Zapisanie korpusu do plików
+
+preprocessedDir <- paste(outputDir, "\\", "preprocessed", sep = "", collapse = NULL)
+dir.create(preprocessedDir)
+writeCorpus(vcorpus, path = preprocessedDir)
+
+# Wyświetlanie informacji o korpusie  --- A
 
 summary(vcorpus)
 inspect(vcorpus[1])
+
+
+# Tworzenie macierzy częstości --- c B-1
+
+tdm_tf_all <- TermDocumentMatrix(vcorpus)
+tdm_bin_all <- TermDocumentMatrix(vcorpus, control = list(weigthing = weightBin))
+
+tdm_tf_1_20 <- TermDocumentMatrix(vcorpus, control = list(bounds = list(global = c(1, Inf))))
+tdm_tfidf_1_20 <- TermDocumentMatrix(vcorpus, control = list( weighting = weightTfIdf, bounds = list(global = c(1, Inf))))
+
+tdm_tf_2_19 <- TermDocumentMatrix(vcorpus, control = list(bounds = list(global = c(2,19))))
+tdm_tfidf_2_19 <- TermDocumentMatrix(vcorpus, control = list( weighting = weightTfIdf, bounds = list(global = c(2,19))))
+
+tdm_tf_3_18 <- TermDocumentMatrix(vcorpus, control = list(bounds = list(global = c(3,18))))
+tdm_tfidf_3_18 <- TermDocumentMatrix(vcorpus, control = list( weighting = weightTfIdf, bounds = list(global = c(3,18))))
+
+tdm_tf_4_17 <- TermDocumentMatrix(vcorpus, control = list(bounds = list(global = c(4,17))))
+tdm_tfidf_4_17 <- TermDocumentMatrix(vcorpus, control = list( weighting = weightTfIdf, bounds = list(global = c(4,17))))
+
+tdm_tf_all
+tdm_bin_all
+
+tdm_tf_1_20
+tdm_tf_2_19
+tdm_tf_3_18
+tdm_tf_4_17
+
+tdm_tfidf_1_20
+tdm_tfidf_2_19
+tdm_tfidf_3_18
+tdm_tfidf_4_17
+
