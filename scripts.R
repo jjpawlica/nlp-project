d04b032676f3c02ebbcb574f4d76755695ed3644
Jakub Pawlica <jakub.pawlica@gmail.com>
2019-06-12 22:12:53 +0200

    naprawa błędów

--- scripts.R
+++ scripts.R
@@ -1,19 +1,24 @@
-# Ustawienie katalogu roboczego i struktury projektu
+# Instalacja pakietów
+
 install.packages("here")
 library(here)
+
+install.packages("lsa")
+library(lsa)
+
+install.packages(c("tm", "hunspell"))
+
+library(tm)
+library(hunspell)
+library(stringr)
+
+# Ustawienie katalogu roboczego i struktury projektu
 
 setwd(here())
 
 inputDir <- ".\\data"
 outputDir <- ".\\results"
 utilsDir <- ".\\utils"
-
-# Instalacja i aktywacja wymaganych pakietów
-install.packages(c("tm", "hunspell"))
-
-library(tm)
-library(hunspell)
-library(stringr)
 
 # Utworzenie korpusu dokumentów (a)
 vcorpus <- VCorpus(
@@ -50,12 +55,16 @@ removeChar <- content_transformer(
 vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(8722), "")
 vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(8217), "")
 vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(8211), "")
+vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(8220), "")
 vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(8221), "")
 vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(8222), "")
+vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(8230), "")
 vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(187), "")
 vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(171), "")
 vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(190), "")
 vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(190), "")
+
+vcorpus <- tm_map(vcorpus, stripWhitespace)
 
 # Wstępne przetwarzanie: lematyzacja
 
@@ -103,26 +112,26 @@ summary(vcorpus)
 inspect(vcorpus[1])
 
 
-# Tworzenie macierzy częstości --- c B-1
+# Tworzenie macierzy częstości --- c 4 b
 
 tdm_tf_all <- TermDocumentMatrix(vcorpus)
 tdm_bin_all <- TermDocumentMatrix(vcorpus, control = list(weigthing = weightBin))
 
 tdm_tf_1_20 <- TermDocumentMatrix(vcorpus, control = list(bounds = list(global = c(1, Inf))))
 tdm_tfidf_1_20 <- TermDocumentMatrix(vcorpus, control = list( weighting = weightTfIdf, bounds = list(global = c(1, Inf))))
-dtm_tfidf_1_20 <- DocumentTermMatrix(vcorpus, control=list(weighting = weightTfIdf, bounds = list(global = c(1,Inf))))
+dtm_tfidf_1_20 <- DocumentTermMatrix(vcorpus, control = list(weighting = weightTfIdf, bounds = list(global = c(1,Inf))))
 
 tdm_tf_2_19 <- TermDocumentMatrix(vcorpus, control = list(bounds = list(global = c(2,19))))
 tdm_tfidf_2_19 <- TermDocumentMatrix(vcorpus, control = list( weighting = weightTfIdf, bounds = list(global = c(2,19))))
-dtm_tfidf_2_19 <- DocumentTermMatrix(vcorpus, control=list(weighting = weightTfIdf, bounds = list(global = c(2,19))))
+dtm_tfidf_2_19 <- DocumentTermMatrix(vcorpus, control = list(weighting = weightTfIdf, bounds = list(global = c(2,19))))
 
 tdm_tf_3_18 <- TermDocumentMatrix(vcorpus, control = list(bounds = list(global = c(3,18))))
 tdm_tfidf_3_18 <- TermDocumentMatrix(vcorpus, control = list( weighting = weightTfIdf, bounds = list(global = c(3,18))))
-dtm_tfidf_3_18 <- DocumentTermMatrix(vcorpus, control=list(weighting = weightTfIdf, bounds = list(global = c(3,18))))
+dtm_tfidf_3_18 <- DocumentTermMatrix(vcorpus, control = list(weighting = weightTfIdf, bounds = list(global = c(3,18))))
 
 tdm_tf_4_17 <- TermDocumentMatrix(vcorpus, control = list(bounds = list(global = c(4,17))))
 tdm_tfidf_4_17 <- TermDocumentMatrix(vcorpus, control = list( weighting = weightTfIdf, bounds = list(global = c(4,17))))
-dtm_tfidf_4_17 <- DocumentTermMatrix(vcorpus, control=list(weighting = weightTfIdf, bounds = list(global = c(4,17))))
+dtm_tfidf_4_17 <- DocumentTermMatrix(vcorpus, control = list(weighting = weightTfIdf, bounds = list(global = c(4,17))))
 
 tdm_tf_all
 tdm_bin_all
@@ -137,8 +146,7 @@ tdm_tfidf_2_19
 tdm_tfidf_3_18
 tdm_tfidf_4_17
 
-
-# Analiza głównych składowych
+# Analiza głównych składowych - d 4 b
 
 pca_1_20 <- prcomp(dtm_tfidf_1_20)
 pca_2_19 <- prcomp(dtm_tfidf_2_19)
@@ -183,7 +191,7 @@ dev.off()
 ## Wykres dla 4 - 17
 pca_plot_4_17 <- paste(outputDir,'\\',"pca_4_17.png",sep = "", collapse = NULL)
 
-png(filename = pca_plot_3_18, width = 1024)
+png(filename = pca_plot_4_17, width = 1024)
 options(scipen = 5)
 par(mar = c(5.1, 4.1, 4.1, 25.1), xpd = TRUE)
 plot(pca_4_17$x[,1], pca_4_17$x[,2], pch = 1, col = "black", xlab = "", ylab = "")
@@ -191,10 +199,39 @@ text(pca_4_17$x[,1], pca_3_18$x[,2], paste("d",1:19,sep = ""), col = "black",pos
 legend("topright",  inset = c(-0.57,0), legend, text.font = 16, cex = 0.5, text.col = "black")
 dev.off()
 
-# Dekompozycja wedłud wartości osobliwych (analiza ukrytych wymiarów semantycznych)
+# Dekompozycja wedłud wartości osobliwych (analiza ukrytych wymiarów semantycznych) d 4 b
 
-install.packages("lsa")
+tdm_tfidf_1_20_matrix <- as.matrix(tdm_tfidf_1_20)
 
-library(lsa)
+lsaModel <- lsa(tdm_tfidf_1_20_matrix)
+lsaModel$tk # odpowiednik macierzy U, współrzędne wyrazów 
+lsaModel$dk # odpowiednik macierzy V, współrzędne dokumentów
+lsaModel$sk # odpowiednik macierzy D, znaczenie składowych
 
-dtm_tfidf_1_20_matrix <- as.matrix(dtm_tfidf_1_20)
+coordTerms <- lsaModel$tk %*% diag(lsaModel$sk)
+coordDocs <- lsaModel$dk %*% diag(lsaModel$sk)
+words <- c("samochód", "auto", "kierowca", "technologia", "przeglądarka", "internet", "serce", 'hiv', "rak", "zdrowie", "dieta", "gen", "dna") 
+coordWords <- coordTerms[words,]
+
+legend <- paste(paste("d",1:20,sep = ""), rownames(coordDocs), sep = " - ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
