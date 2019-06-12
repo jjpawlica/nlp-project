28a431fff2f0aac14cedd39f99529c07704b53d6
Jakub Pawlica <jakub.pawlica@gmail.com>
2019-06-12 15:30:58 +0200

    zmiana kodowania na utf-8

--- scripts.R
+++ scripts.R
@@ -13,3 +13,30 @@ install.packages(c("tm", "hunspell"))
 library(tm)
 library(hunspell)
 library(stringr)
+
+# Utworzenie korpusu dokumentów
+vcorpus <- VCorpus(
+  DirSource(inputDir, pattern = "*.txt", encoding = "UTF-8"),
+  readerControl = list(language = "pl_PL")
+)
+
+
+# Wstępne przetwarzanie
+
+vcorpus <- tm_map(vcorpus, removeNumbers)
+vcorpus <- tm_map(vcorpus, removePunctuation)
+vcorpus <- tm_map(vcorpus, content_transformer(tolower))
+
+# Wyświetlanie informacji o korpusie
+vcorpus
+summary(vcorpus)
+View(vcorpus)
+inspect(vcorpus)
+
+vcorpus[1]
+summary(vcorpus[1])
+inspect(vcorpus[1])
+
+vcorpus[[1]]
+summary(vcorpus[[1]])
+inspect(vcorpus[[1]])
