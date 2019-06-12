261fba129149564066cf6262a89eba37a5fbc4b2
Jakub Pawlica <jakub.pawlica@gmail.com>
2019-06-12 17:42:22 +0200

    naprawione teksty i funkcja lematyzacji

--- scripts.R
+++ scripts.R
@@ -15,30 +15,72 @@ library(tm)
 library(hunspell)
 library(stringr)
 
-# Utworzenie korpusu dokumentów
+# Utworzenie korpusu dokumentów (a)
 vcorpus <- VCorpus(
   DirSource(inputDir, pattern = "*.txt", encoding = "UTF-8"),
   readerControl = list(language = "pl_PL")
 )
 
-# Wstępne przetwarzanie: numery, znaki interpunkcyjne, do małej litery, 
+# Wstępne przetwarzanie: nadmierne puste znaki (b)
 
+vcorpus <- tm_map(vcorpus, stripWhitespace)
+
+# Wstępne przetwarzanie: numery, znaki interpunkcyjne, do małej litery (b)
+
+vcorpus <- tm_map(vcorpus, content_transformer(tolower))
 vcorpus <- tm_map(vcorpus, removeNumbers)
 vcorpus <- tm_map(vcorpus, removePunctuation)
-vcorpus <- tm_map(vcorpus, content_transformer(tolower))
 
 
-# Wstępne przetwarzanie: słowa ze stop listy
+# Wstępne przetwarzanie: słowa ze stop listy (b)
 
 stoplistFile <- paste(utilsDir, "\\", "stopwords_pl.txt", sep = "", collapse = NULL)
 stoplist <- readLines(stoplistFile, encoding = "UTF-8")
 vcorpus <- tm_map(vcorpus, removeWords, stoplist)
 
-# Wstępne przetwarzanie: nadmierne puste znaki
-
 vcorpus <- tm_map(vcorpus, stripWhitespace)
+vcorpus <- tm_map(vcorpus, removePunctuation)
+
+# Wstępne przetwarzanie: usunięcie niechcianych znaków
+
+removeChar <- content_transformer(
+  function(x, pattern, replacement) gsub(pattern, replacement, x)
+)
+
+vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(8722), "")
+vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(8217), "")
+vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(8211), "")
+vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(8221), "")
+vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(8222), "")
+vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(187), "")
+vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(171), "")
+vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(190), "")
+vcorpus <- tm_map(vcorpus,removeChar, intToUtf8(190), "")
+
+
+
+# Wstępne przetwarzanie: lematyzacja
+
+polish <- dictionary(lang = "pl_PL", affix = NULL, cache = TRUE)
+
+lemmatize <- function(text) {
+  singleText <- str_trim(as.character(paste(text,collapse = "")))
+  parsedText <- strsplit(singleText, " ")
+
+  newTextVec <- hunspell_stem(parsedText[[1]], dict = polish)
+  
+  newTextVec
+    for (i in 1:length(newTextVec)) {
+     if (length(newTextVec[[i]]) == 0) newTextVec[i] <- parsedText[[1]][i]
+     if (length(newTextVec[[i]]) > 1) newTextVec[i] <- newTextVec[[i]][1]
+   }
+  newText <- paste(newTextVec, collapse = " ")
+  return(newText)
+}
+
+vcorpus <- tm_map(vcorpus, content_transformer(lemmatize))
 
 # Wyświetlanie informacji o korpusie
 
 summary(vcorpus)
-inspect(vcorpus)
+inspect(vcorpus[1])
